fddb1991c0b6091d509eb9ae523f3f1d27bf06b8
gerkra <gerkra92@gmail.com>
2015-05-06 16:54:52 +0300

    style

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,7 +1,18 @@
-/**
- * Created by German on 21.04.2015.
- */
 $(document).ready(function () {
+
+    smooth();
+
+    $(document).mousemove(function(e){
+        var Y = e.pageY;
+        if (Y >= 260 && $('.second .sub-menu').height() == 220) {
+            $('.second .sub-menu').stop().animate({height: 0}, 500, function () {
+                $('#header').height(120);
+                $('.second').removeClass('open');
+                $('.second .navigation').find('.active').removeClass('active');
+            });
+        }
+    });
+
     $(document).scroll(function () {
         var heightHead = $("#header").height();
         var posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
@@ -15,46 +26,27 @@ $(document).ready(function () {
         }
     });
 
-    smooth(300, 12000);
-
     $('.second .menu > li >a').hover(function (e) {
         $('.second').addClass('open');
         $('#header').height(260);
         $('.second .navigation').find('.active').removeClass('active');
         e.currentTarget.className = 'active';
-
         $(e.target).parent('li').find('ul').addClass('active');
-
-
-        $('.second .sub-menu').stop().animate({height: 220}, 500);
-    }, function () {
-
+        if ($('.second .sub-menu').height() != 220) {
+            $('.second .sub-menu').stop().animate({height: 220}, 500);
+        }
     });
 
-    $('#header').hover(function () {
-
-    }, function () {
-
-        $('.second .sub-menu').stop().animate({height: 0}, 500, function () {
-            $('#header').height(120);
-            $('.second').removeClass('open');
-            $('.second .navigation').find('.active').removeClass('active');
-        });
-
-    });
-
-    function smooth(orientation, time) {
-        var start = $('#slider img').css('right');
+    function smooth() {
+        var width = $('#slider').width();
         $('#slider img').stop().animate({
-            right: '+=' + orientation
-        }, time, "linear", function () {
-
+            left: 50
+        }, width * 25, "linear", function () {
             $('#slider img').stop().animate({
-                right: '-=' + orientation
-            }, time, "linear", function () {
-                smooth(orientation, time);
+                left: width - 450
+            }, width * 25, "linear", function () {
+                smooth();
             });
         });
-
     }
 });
