1be40b570b198b29faa48cb1cb7a1a9004fa918f
gerkra <gerkra92@gmail.com>
2015-05-07 14:46:07 +0300

    style

--- templates/js/main.js
+++ templates/js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
 
     smooth();
 
+    $( window ).resize(function() {
+        smooth();
+    });
+
     $(document).mousemove(function(e){
         var Y = e.pageY;
         if (Y >= 260 && $('.second .sub-menu').height() == 220) {
@@ -38,15 +42,25 @@ $(document).ready(function () {
     });
 
     function smooth() {
-        var width = $('#slider').width();
-        $('#slider img').stop().animate({
-            left: 50
-        }, width * 25, "linear", function () {
-            $('#slider img').stop().animate({
-                left: width - 450
-            }, width * 25, "linear", function () {
-                smooth();
+        $('#slider img').stop();
+        if ($(window).width() > 768) {
+            $('#slider').find('img').css({
+                left: $(window).width() - $('#slider').find('img').width() - 50
             });
-        });
+            var width = $('#slider').width();
+            $('#slider img').stop().animate({
+                left: 50
+            }, width * 25, "linear", function () {
+                $('#slider img').stop().animate({
+                    left: width - 450
+                }, width * 25, "linear", function () {
+                    smooth();
+                });
+            });
+        } else {
+            $('#slider').find('img').css({
+                left: ($(window).width() - 300) / 2
+            });
+        }
     }
 });
